23c6f9e04e6fe52d7dbbdfa9c958ebb397d379c0
魂狩 <a364717646@163.com>
2016-01-14 09:47:46 +0800

    restful

--- application/config/config.php
+++ application/config/config.php
@@ -27,6 +27,16 @@ $config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+|Whether CI uses restful design model.
+|
+*/
+$config['restful'] = TRUE;
+
+/*
+|--------------------------------------------------------------------------
 | Index File
 |--------------------------------------------------------------------------
 |
@@ -315,65 +325,6 @@ $config['cache_query_string'] = FALSE;
 |
 */
 $config['encryption_key'] = '';
-
-/*
-|--------------------------------------------------------------------------
-| Session Variables
-|--------------------------------------------------------------------------
-|
-| 'sess_driver'
-|
-|	The storage driver to use: files, database, redis, memcached
-|
-| 'sess_cookie_name'
-|
-|	The session cookie name, must contain only [0-9a-z_-] characters
-|
-| 'sess_expiration'
-|
-|	The number of SECONDS you want the session to last.
-|	Setting to 0 (zero) means expire when the browser is closed.
-|
-| 'sess_save_path'
-|
-|	The location to save sessions to, driver dependent.
-|
-|	For the 'files' driver, it's a path to a writable directory.
-|	WARNING: Only absolute paths are supported!
-|
-|	For the 'database' driver, it's a table name.
-|	Please read up the manual for the format with other session drivers.
-|
-|	IMPORTANT: You are REQUIRED to set a valid save path!
-|
-| 'sess_match_ip'
-|
-|	Whether to match the user's IP address when reading the session data.
-|
-|	WARNING: If you're using the database driver, don't forget to update
-|	         your session table's PRIMARY KEY when changing this setting.
-|
-| 'sess_time_to_update'
-|
-|	How many seconds between CI regenerating the session ID.
-|
-| 'sess_regenerate_destroy'
-|
-|	Whether to destroy session data associated with the old session ID
-|	when auto-regenerating the session ID. When set to FALSE, the data
-|	will be later deleted by the garbage collector.
-|
-| Other session cookie settings are shared with the rest of the application,
-| except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
-|
-*/
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
