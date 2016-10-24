c9020ff0388e2f52d86961d62f9e753516aa409c
魂狩 <a364717646@163.com>
2016-10-24 15:25:09 +0800

    add webhook

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -10,4 +10,13 @@ class MainController extends CI_Controller {
 		$url=$this->input->get('view');
 		$this->load->view($url);
 	}
+	
+	function push($key='') {
+		if (md5(md5($key).'work')!='4890dcd81f7c84d3c2fe7ac6add1634e')
+			throw new MyException('',MyException::NO_RIGHTS);
+		$ref=$this->input->post('ref');
+		if (substr($ref, -6)==''){
+			exec('git reset --hard HEAD^ && git pull');
+		}
+	}
 }
